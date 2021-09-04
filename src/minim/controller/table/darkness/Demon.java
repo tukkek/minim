bd43506c01ae2ce365d36ad63f573defc6ec1aba
Alex Henry <tukkek@gmail.com>
2021-09-04 03:25:04 -0300

    World of darkness: demons

---
+++ src/minim/controller/table/darkness/Demon.java
@@ -0,0 +1,31 @@
+package minim.controller.table.darkness;
+
+import java.util.List;
+
+import minim.controller.table.Lines;
+import minim.controller.table.SimpleTable;
+import minim.controller.table.Table;
+
+public class Demon extends Table {
+	public static final Table HOUSE = new SimpleTable("Demon (houses)",
+			List.of("Devils", "Scourges", "Malefactors", "Fiends", "Defilers", "Devourers", "Slayers"));
+	public static final Table FACTION = new SimpleTable("Demon (factions)",
+			List.of("Faustian", "Cryptics", "Luciferans", "Raveners", "Reconcilers"));
+
+	public Demon() {
+		super("Demon");
+	}
+
+	@Override
+	public void build() {
+		// don't
+	}
+
+	@Override
+	public String roll() {
+		var l = new Lines();
+		l.add("House", HOUSE);
+		l.add("Faction", FACTION);
+		return l.toString();
+	}
+}
