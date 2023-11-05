ce54797f05351eb854e63e043522a709efa53e3f
Alex Henry <tukkek@gmail.com>
2023-11-05 00:00:00 +0000

    Hexcrawl: compact output

--- src/minim/controller/table/hexcrawl/Hex.java
+++ src/minim/controller/table/hexcrawl/Hex.java
@@ -5,79 +5,82 @@ import java.util.List;
 import minim.controller.table.MetaTable.SimpleMetaTable;
 import minim.controller.table.Table;
 
-public class Hex extends Table {
-	public static final Hex SINGLETON = new Hex();
-	public static final Table NEXT = new SimpleMetaTable("Hex (next)",
-			List.of(Elevation.NEXT, Vegetation.NEXT, Water.NEXT, PointOfInterest.SINGLETON));
-	static final List<String> CHARACTERISTICS = List.of("Elevation", "Vegetation", "Water", "Point of interest");
+public class Hex extends Table{
+  public static final Hex SINGLETON=new Hex();
+  public static final Table NEXT=new SimpleMetaTable("Hex (next)",List
+      .of(Elevation.NEXT,Vegetation.NEXT,Water.NEXT,PointOfInterest.SINGLETON));
+  static final List<String> CHARACTERISTICS=List.of("Elevation","Vegetation",
+      "Water","Point of interest");
 
-	public static class Hexagon {
-		String feature = PointOfInterest.SINGLETON.roll();
-		String elevation;
-		String vegetation;
-		String water;
+  public static class Hexagon{
+    String feature=PointOfInterest.SINGLETON.roll();
+    String elevation;
+    String vegetation;
+    String water;
 
-		protected Hexagon(String elevationp, String vegetationp, String waterp) {
-			elevation = elevationp;
-			vegetation = vegetationp;
-			water = waterp;
-			if (interest()) {
-				feature = PointOfInterest.TYPES.roll();
-			}
-		}
+    protected Hexagon(String elevationp,String vegetationp,String waterp){
+      elevation=elevationp;
+      vegetation=vegetationp;
+      water=waterp;
+      if(interest()) feature=PointOfInterest.TYPES.roll();
+    }
 
-		public Hexagon() {
-			this(Elevation.SINGLETON.roll(), Vegetation.SINGLETON.roll(), Water.SINGLETON.roll());
-		}
+    public Hexagon(){
+      this(Elevation.SINGLETON.roll(),Vegetation.SINGLETON.roll(),
+          Water.SINGLETON.roll());
+    }
 
-		public boolean interest() {
-			return feature != PointOfInterest.NONE;
-		}
+    public boolean interest(){
+      return feature!=PointOfInterest.NONE;
+    }
 
-		@Override
-		public String toString() {
-			var s = vegetation + " " + elevation.toLowerCase();
-			if (water != Water.DRY)
-				s += " by " + water.toLowerCase();
-			if (interest())
-				s += " (point of interest: " + feature.toLowerCase() + ")";
-			return s;
-		}
+    public String toString(boolean full){
+      var s=vegetation + " " + elevation.toLowerCase();
+      if(water!=Water.DRY) s+=" by " + water.toLowerCase();
+      if(full&&interest())
+        s+=" (point of interest: " + feature.toLowerCase() + ")";
+      return s;
+    }
 
-		static String next(String current, Table t, Table next, String lower, String higher) {
-			var nexte = next.roll();
-			var l = t.lines;
-			var i = l.indexOf(current);
-			if (nexte == higher && i > 0) {
-				i -= 1;
-			} else if (nexte == lower && i < l.size() - 1) {
-				i += 1;
-			}
-			return l.get(i);
-		}
+    @Override
+    public String toString(){
+      return toString(true);
+    }
 
-		public Hexagon next() {
-			var e = next(elevation, Elevation.SINGLETON, Elevation.NEXT, Elevation.LOWER, Elevation.HIGHER);
-			var v = next(vegetation, Vegetation.SINGLETON, Vegetation.NEXT, Vegetation.SPARSER, Vegetation.DENSER);
-			var w = next(water, Water.SINGLETON, Water.NEXT, Water.WETTER, Water.DRYER);
-			return new Hexagon(e, v, w);
-		}
-	}
+    static String next(String current,Table t,Table next,String lower,
+        String higher){
+      var nexte=next.roll();
+      var l=t.lines;
+      var i=l.indexOf(current);
+      if(nexte==higher&&i>0) i-=1;
+      else if(nexte==lower&&i<l.size()-1) i+=1;
+      return l.get(i);
+    }
 
-	Hex() {
-		super("Hex");
-	}
+    public Hexagon next(){
+      var e=next(elevation,Elevation.SINGLETON,Elevation.NEXT,Elevation.LOWER,
+          Elevation.HIGHER);
+      var v=next(vegetation,Vegetation.SINGLETON,Vegetation.NEXT,
+          Vegetation.SPARSER,Vegetation.DENSER);
+      var w=next(water,Water.SINGLETON,Water.NEXT,Water.WETTER,Water.DRYER);
+      return new Hexagon(e,v,w);
+    }
+  }
 
-	@Override
-	public void build() {
-		// don't
-	}
+  Hex(){
+    super("Hex");
+  }
 
-	@Override
-	public String roll() {
+  @Override
+  public void build(){
+    // don't
+  }
+
+  @Override
+  public String roll(){
 //		return roll(List.of(Elevation.SINGLETON, Vegetation.SINGLETON, Water.SINGLETON, PointOfInterest.SINGLETON));
-		return new Hexagon().toString();
-	}
+    return new Hexagon().toString();
+  }
 
 //	static String roll(List<Table> tables) {
 //		var rolls = new LinkedList<>(tables.stream().map(t -> t.roll()).collect(Collectors.toList()));
--- src/minim/controller/table/hexcrawl/Hexcrawl.java
+++ src/minim/controller/table/hexcrawl/Hexcrawl.java
@@ -1,59 +1,103 @@
 package minim.controller.table.hexcrawl;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import minim.controller.table.Table;
 import minim.controller.table.hexcrawl.Hex.Hexagon;
 
 /** TODO hexagonal map */
-public class Hexcrawl extends Table {
-	public static final Hexcrawl SMALL = new Hexcrawl(3);
-	public static final Hexcrawl BIG = new Hexcrawl(5);
+public class Hexcrawl extends Table{
+  public static final Hexcrawl SMALL=new Hexcrawl(3);
+  public static final Hexcrawl BIG=new Hexcrawl(5);
 
-	String letters = "abcdefghijklmnopqrstuvwxyz";
-	int size;
+  static final int LINE=100;
 
-	Hexcrawl(int size) {
-		super("Hexcrawl (%sx%s)".formatted(size, size));
-		this.size = size;
-	}
+  String letters="abcdefghijklmnopqrstuvwxyz";
+  int size;
 
-	@Override
-	public void build() {
-		// don't
-	}
+  Hexcrawl(int size){
+    super("Hexcrawl (%sx%s)".formatted(size,size));
+    this.size=size;
+  }
 
-	public String roll() {
-		Hexagon[][] map = new Hexagon[size][size];
-		List<Hexagon> hexes = new ArrayList<>(size * size);
-		HashMap<Hexagon, Character> legend = new HashMap<>(size * size);
-		var ascii = "";
-		var description = "";
-		for (var y = 0; y < size; y++) {
-			var shorter = y % 2 == 1;
-			if (shorter)
-				ascii += "  ";
-			for (var x = 0; x < size - (shorter ? 1 : 0); x++) {
-				Hexagon h;
-				if (x == 0 && y == 0)
-					h = new Hexagon();
-				else if (x == 0 && y > 0)
-					h = map[0][y - 1].next();
-				else
-					h = map[x - 1][y].next();
-				map[x][y] = h;
-				hexes.add(h);
-				var l = letters.charAt(legend.size());
-				if (h.interest())
-					l = Character.toUpperCase(l);
-				legend.put(h, l);
-				ascii += l + "  ";
-				description += "%s = %s\n".formatted(l, h.toString());
-			}
-			ascii += "\n";
-		}
-		return ascii + "\n" + description;
-	}
+  @Override
+  public void build(){
+    // don't
+  }
+
+  String join(List<String> map){
+    return String.join("\n",map);
+  }
+
+  String space(List<String> chunks){
+    chunks=new ArrayList<>(chunks);
+    var blank=LINE-chunks.stream().mapToInt(c -> c.length()).sum();
+    chunks.add(0,"");
+    var space=blank / chunks.size();
+    if(space<1) space=1;
+    return String.join(" ".repeat(space),chunks);
+  }
+
+  String spread(List<String> labels){
+    var nlabels=labels.size();
+    var lines=new ArrayList<String>(nlabels / 2);
+    List<String> line=null;
+    for (var l : labels){
+      if(line==null){
+        line=new ArrayList<>(List.of(l));
+        continue;
+      }
+      if(line.stream().mapToInt(chunk -> chunk.length()+1).sum()
+          +l.length()>LINE){
+        lines.add(space(line));
+        line=new ArrayList<>(1);
+      }
+      line.add(l);
+    }
+    if(!line.isEmpty()) lines.add(space(line));
+    return join(lines);
+  }
+
+  String print(List<String> labels,List<String> map,
+      Map<Hexagon,Character> hexes){
+    var interesting=hexes.keySet().stream().filter(h -> h.interest()).toList();
+    interesting=new ArrayList<>(interesting);
+    var poi=interesting.stream()
+        .sorted(Comparator.comparing(i -> Character.toLowerCase(hexes.get(i))))
+        .map(i -> "%s = %s".formatted(hexes.get(i),i.feature)).toList();
+    return "%s\n\n%s\n\n%s".formatted(join(map),spread(poi),spread(labels));
+  }
+
+  @Override
+  public String roll(){
+    var legend=new HashMap<Hexagon,Character>(size * size);
+    var hexes=new ArrayList<Hexagon>(size * size);
+    var labels=new ArrayList<String>();
+    var crawl=new Hexagon[size][size];
+    var map=new ArrayList<String>();
+    for (var y=0; y<size; y++){
+      var shorter=y % 2==1;
+      var line=shorter?"  ":"";
+      for (var x=0; x<size-(shorter?1:0); x++){
+        Hexagon h;
+        if(x==0&&y==0) h=new Hexagon();
+        else if(x==0&&y>0) h=crawl[0][y-1].next();
+        else h=crawl[x-1][y].next();
+        crawl[x][y]=h;
+        hexes.add(h);
+        var l=letters.charAt(legend.size());
+        if(h.interest()) l=Character.toUpperCase(l);
+        legend.put(h,l);
+        line+=l + "  ";
+        labels.add(
+            "%s = %s".formatted(Character.toLowerCase(l),h.toString(false)));
+      }
+      map.add(line);
+    }
+    return print(labels,map,legend);
+  }
 }
