d268dd86a79f8c5591c12fe753127440dd0f75a2
Alex Henry <tukkek@gmail.com>
2023-11-11 00:00:00 +0000

    Combat table

--- web/script/control/table/tables.js
+++ web/script/control/table/tables.js
@@ -1,7 +1,8 @@
 import * as adungeon from './adventure/dungeon.js'
 import * as type from './adventure/type.js'
 import * as bold from './bold.js'
+import * as combat from './combat.js'
 
-export var tables=[adungeon,type,bold]
+export var tables=[adungeon,type,bold,combat]
 
 export function setup(){tables=tables.flatMap(t=>t.tables)}
