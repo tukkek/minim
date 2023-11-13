ea4d7aedc3549256d59dc7b2522c720ad4d5b8da
Alex Henry <tukkek@gmail.com>
2023-11-13 00:00:00 +0000

    Wraith tables

--- web/script/control/table/darkness/darkness.js
+++ web/script/control/table/darkness/darkness.js
@@ -3,6 +3,8 @@ import * as mage from './mage.js'
 import * as werewolf from './werewolf.js'
 import * as table from '../table.js'
 
+export var tables=[kindred,mage,werewolf].flatMap(t=>t.tables)
+                    
 const FAMILIES = new table.Table("Darkness, beast, family", ["Anakim, giants", "Eshmaki, lurkers",
     "Inguma, outsiders", "Makara, leviathans", "Namtaru, gorgoms", "Talassii, captors", "Ugallu, raptors"])
 const HUNGER = new table.Table("Darkness, beast, hunger",
@@ -23,5 +25,37 @@ class Beast extends table.Table {
 	}
 }
 
-export var tables=[kindred,mage,werewolf].flatMap(t=>t.tables)
-                    .concat([FAMILIES,HUNGER,new Beast()])
+tables.push(...[FAMILIES,HUNGER,new Beast()])
+
+const LEGIONS = new table.Table("Darkness, wraith, faction, hierarchy legions",
+  ["Emerald legion", "Legion of fate", "Grim legion", "Iron legion", "Legion of paupers",
+      "Penitent legion", "Silent legion", "Skeletal legion"])
+const HERETICS = new table.Table("Darkness, wraith, faction, heretics",
+  ["The host", "Children of the green", "Riders of the wheel"])
+const RENEGADES = new table.Table("Darkness, wraith, faction, renegades",
+  ["Protesters", "Idealists", "Outlaws", "Drop-outs"])
+const FACTIONS = new table.Table("Darkness, wraith, faction")
+FACTIONS.add(LEGIONS,2);
+FACTIONS.add(HERETICS);
+FACTIONS.add(RENEGADES);
+FACTIONS.add("Ferrymen", 1);
+
+const GUILDS = new table.Table("Darkness, wraith, guilds",
+  ["Alchemists", "Artificers", "Chanteur", "Harbingers", "Haunters", "Masquers", "Mnemoi", "Monitors",
+      "Oracles", "Pardoners", "Proctors", "Sandmen", "Solicitors", "Sppoks", "Usurers"])
+
+class Wraith extends table.Table {
+	constructor() {
+		super("Darkness, wraith");
+	}
+
+  roll() {
+		let l = [
+      "Faction: "+ FACTIONS,
+      "Guild: "+ GUILDS
+    ]
+		return l.join('<br/>')
+	}
+}
+
+tables.push(...[LEGIONS,HERETICS,RENEGADES,FACTIONS,GUILDS,new Wraith()])
