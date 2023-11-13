ec7dc5d967d5a68d4fe5b6c5d348903af90a42f1
Alex Henry <tukkek@gmail.com>
2023-11-13 00:00:00 +0000

    Beast tables

--- web/script/control/table/darkness/darkness.js
+++ web/script/control/table/darkness/darkness.js
@@ -1,5 +1,27 @@
 import * as kindred from './kindred.js'
 import * as mage from './mage.js'
 import * as werewolf from './werewolf.js'
+import * as table from '../table.js'
+
+const FAMILIES = new table.Table("Darkness, beast, family", ["Anakim, giants", "Eshmaki, lurkers",
+    "Inguma, outsiders", "Makara, leviathans", "Namtaru, gorgoms", "Talassii, captors", "Ugallu, raptors"])
+const HUNGER = new table.Table("Darkness, beast, hunger",
+    ["Predator (prey)", "Tyrant (power)", "Nemesies (punishment)", "Ravager (ruin)",
+        "Collectors (hoard)", "Whispers (secrets)", "Enablers (transgression)"])
+
+class Beast extends table.Table {
+	constructor() {
+		super("Darkness, beast");
+	}
+
+	roll() {
+		let l = [
+      "Family: "+ FAMILIES,
+      "Hunger: "+ HUNGER
+    ]
+		return l.join('<br/>');
+	}
+}
 
 export var tables=[kindred,mage,werewolf].flatMap(t=>t.tables)
+                    .concat([FAMILIES,HUNGER,new Beast()])
