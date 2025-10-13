ea3daa8f54d77571e7614f718360cf7ba85b83f0
Alex Henry <tukkek@gmail.com>
2025-10-13 00:00:00 -0300

    Inline external Table module.

--- docs/script/control/table/table.js
+++ docs/script/control/table/table.js
@@ -1,6 +1,28 @@
-import * as tablem from '../../../lib/table/table.js'
+class BaseTable{//TODO inlined from external-module
+  constructor(name,rows=[]){
+    if(!name) throw new Error('Table needs name')
+    this.name=name
+    this.rows=rows//[object...]
+  }
 
-export class Table extends tablem.Table{
+  roll(){
+    let row=Table.pick(this.rows)
+    return row instanceof Table?row.roll():row
+  }
+
+  add(row,count=1){
+    count=Math.floor(count)
+    for(let i=0;i<count;i++) this.rows.push(row)
+  }
+
+  toString(){return this.roll()}
+
+  get distinct(){return new Set(this.rows)}
+
+  static pick(array){return array[Math.floor(Math.random()*array.length)]}
+}
+
+export class Table extends BaseTable{
   constructor(name,rows=[]){
     super(name,rows)
     this.end='.'
